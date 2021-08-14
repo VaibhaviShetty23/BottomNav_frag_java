ff896f444e8ec977903ea0379161f7e6437440b9
{Vaibhavi} <{vaibhavishettyme123@gmail.com}>
2021-08-14 14:51:03 +0530

    First

---
+++ app/src/main/java/com/example/bottomnav_frag_java/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.bottomnav_frag_java;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemSelectedListener;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationBarView.OnItemSelectedListener;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigationView = findViewById(R.id.btm_nav);
+        navigationView.setOnNavigationItemSelectedListener();
+
+
+    }
+}
